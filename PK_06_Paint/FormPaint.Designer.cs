c306c3dcc6f31650303462c8662666e3669bdf0a
Sebastian Wojczyk <sebastian.wojczyk@wmii.uni.lodz.pl>
2023-03-31 13:52:49 +0200

    Gotowy Paint z historią i podglądem podczas rysowania.

---
+++ PK_06_Paint/FormPaint.Designer.cs
@@ -0,0 +1,385 @@
+﻿namespace PK_06_Paint
+{
+    partial class FormPaint
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBoxFill = new System.Windows.Forms.CheckBox();
+            this.buttonColorFill = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numericUpDownWidthPen = new System.Windows.Forms.NumericUpDown();
+            this.buttonColorPen = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.radioButtonCurve = new System.Windows.Forms.RadioButton();
+            this.radioButtonEllipse = new System.Windows.Forms.RadioButton();
+            this.radioButtonRectangle = new System.Windows.Forms.RadioButton();
+            this.radioButtonLine = new System.Windows.Forms.RadioButton();
+            this.pictureBoxImage = new System.Windows.Forms.PictureBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.plikToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.nowyToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.otwórzToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.zapiszToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.edycjaToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cofnijToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.ponówToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidthPen)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).BeginInit();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Absolute, 150F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Controls.Add(this.panel1, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.pictureBoxImage, 1, 0);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 24);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 1;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 100F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(862, 433);
+            this.tableLayoutPanel1.TabIndex = 14;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.groupBox3);
+            this.panel1.Controls.Add(this.groupBox2);
+            this.panel1.Controls.Add(this.groupBox1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(3, 3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(144, 427);
+            this.panel1.TabIndex = 0;
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.label1);
+            this.groupBox3.Controls.Add(this.checkBoxFill);
+            this.groupBox3.Controls.Add(this.buttonColorFill);
+            this.groupBox3.Location = new System.Drawing.Point(3, 209);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(141, 72);
+            this.groupBox3.TabIndex = 2;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Pędzel";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(63, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(31, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Kolor";
+            // 
+            // checkBoxFill
+            // 
+            this.checkBoxFill.AutoSize = true;
+            this.checkBoxFill.Location = new System.Drawing.Point(44, 48);
+            this.checkBoxFill.Name = "checkBoxFill";
+            this.checkBoxFill.Size = new System.Drawing.Size(72, 17);
+            this.checkBoxFill.TabIndex = 3;
+            this.checkBoxFill.Text = "Tak / Nie";
+            this.checkBoxFill.UseVisualStyleBackColor = true;
+            // 
+            // buttonColorFill
+            // 
+            this.buttonColorFill.BackColor = System.Drawing.Color.SpringGreen;
+            this.buttonColorFill.Location = new System.Drawing.Point(6, 19);
+            this.buttonColorFill.Name = "buttonColorFill";
+            this.buttonColorFill.Size = new System.Drawing.Size(51, 23);
+            this.buttonColorFill.TabIndex = 2;
+            this.buttonColorFill.UseVisualStyleBackColor = false;
+            this.buttonColorFill.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.numericUpDownWidthPen);
+            this.groupBox2.Controls.Add(this.buttonColorPen);
+            this.groupBox2.Location = new System.Drawing.Point(3, 123);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(141, 80);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Pióro";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(64, 52);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Grubość";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(64, 25);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Kolor";
+            // 
+            // numericUpDownWidthPen
+            // 
+            this.numericUpDownWidthPen.Location = new System.Drawing.Point(7, 50);
+            this.numericUpDownWidthPen.Maximum = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDownWidthPen.Minimum = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            this.numericUpDownWidthPen.Name = "numericUpDownWidthPen";
+            this.numericUpDownWidthPen.Size = new System.Drawing.Size(51, 20);
+            this.numericUpDownWidthPen.TabIndex = 1;
+            this.numericUpDownWidthPen.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // buttonColorPen
+            // 
+            this.buttonColorPen.BackColor = System.Drawing.Color.Black;
+            this.buttonColorPen.Location = new System.Drawing.Point(7, 20);
+            this.buttonColorPen.Name = "buttonColorPen";
+            this.buttonColorPen.Size = new System.Drawing.Size(51, 23);
+            this.buttonColorPen.TabIndex = 0;
+            this.buttonColorPen.UseVisualStyleBackColor = false;
+            this.buttonColorPen.Click += new System.EventHandler(this.buttonColor_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.radioButtonCurve);
+            this.groupBox1.Controls.Add(this.radioButtonEllipse);
+            this.groupBox1.Controls.Add(this.radioButtonRectangle);
+            this.groupBox1.Controls.Add(this.radioButtonLine);
+            this.groupBox1.Location = new System.Drawing.Point(3, 3);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(141, 114);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Kształt";
+            // 
+            // radioButtonCurve
+            // 
+            this.radioButtonCurve.AutoSize = true;
+            this.radioButtonCurve.Location = new System.Drawing.Point(6, 88);
+            this.radioButtonCurve.Name = "radioButtonCurve";
+            this.radioButtonCurve.Size = new System.Drawing.Size(59, 17);
+            this.radioButtonCurve.TabIndex = 3;
+            this.radioButtonCurve.Text = "Krzywa";
+            this.radioButtonCurve.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonEllipse
+            // 
+            this.radioButtonEllipse.AutoSize = true;
+            this.radioButtonEllipse.Location = new System.Drawing.Point(6, 65);
+            this.radioButtonEllipse.Name = "radioButtonEllipse";
+            this.radioButtonEllipse.Size = new System.Drawing.Size(53, 17);
+            this.radioButtonEllipse.TabIndex = 2;
+            this.radioButtonEllipse.Text = "Elipsa";
+            this.radioButtonEllipse.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonRectangle
+            // 
+            this.radioButtonRectangle.AutoSize = true;
+            this.radioButtonRectangle.Location = new System.Drawing.Point(6, 42);
+            this.radioButtonRectangle.Name = "radioButtonRectangle";
+            this.radioButtonRectangle.Size = new System.Drawing.Size(70, 17);
+            this.radioButtonRectangle.TabIndex = 1;
+            this.radioButtonRectangle.Text = "Prostokąt";
+            this.radioButtonRectangle.UseVisualStyleBackColor = true;
+            // 
+            // radioButtonLine
+            // 
+            this.radioButtonLine.AutoSize = true;
+            this.radioButtonLine.Checked = true;
+            this.radioButtonLine.Location = new System.Drawing.Point(6, 19);
+            this.radioButtonLine.Name = "radioButtonLine";
+            this.radioButtonLine.Size = new System.Drawing.Size(55, 17);
+            this.radioButtonLine.TabIndex = 0;
+            this.radioButtonLine.TabStop = true;
+            this.radioButtonLine.Text = "Prosta";
+            this.radioButtonLine.UseVisualStyleBackColor = true;
+            // 
+            // pictureBoxImage
+            // 
+            this.pictureBoxImage.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBoxImage.Location = new System.Drawing.Point(153, 3);
+            this.pictureBoxImage.Name = "pictureBoxImage";
+            this.pictureBoxImage.Size = new System.Drawing.Size(706, 427);
+            this.pictureBoxImage.TabIndex = 1;
+            this.pictureBoxImage.TabStop = false;
+            this.pictureBoxImage.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBoxImage_MouseDown);
+            this.pictureBoxImage.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBoxImage_MouseMove);
+            this.pictureBoxImage.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBoxImage_MouseUp);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.plikToolStripMenuItem,
+            this.edycjaToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(862, 24);
+            this.menuStrip1.TabIndex = 15;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // plikToolStripMenuItem
+            // 
+            this.plikToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.nowyToolStripMenuItem,
+            this.otwórzToolStripMenuItem,
+            this.zapiszToolStripMenuItem});
+            this.plikToolStripMenuItem.Name = "plikToolStripMenuItem";
+            this.plikToolStripMenuItem.Size = new System.Drawing.Size(38, 20);
+            this.plikToolStripMenuItem.Text = "Plik";
+            // 
+            // nowyToolStripMenuItem
+            // 
+            this.nowyToolStripMenuItem.Name = "nowyToolStripMenuItem";
+            this.nowyToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.nowyToolStripMenuItem.Text = "Nowy";
+            this.nowyToolStripMenuItem.Click += new System.EventHandler(this.nowyToolStripMenuItem_Click);
+            // 
+            // otwórzToolStripMenuItem
+            // 
+            this.otwórzToolStripMenuItem.Name = "otwórzToolStripMenuItem";
+            this.otwórzToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.otwórzToolStripMenuItem.Text = "Otwórz";
+            this.otwórzToolStripMenuItem.Click += new System.EventHandler(this.otwórzToolStripMenuItem_Click);
+            // 
+            // zapiszToolStripMenuItem
+            // 
+            this.zapiszToolStripMenuItem.Name = "zapiszToolStripMenuItem";
+            this.zapiszToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.zapiszToolStripMenuItem.Text = "Zapisz";
+            this.zapiszToolStripMenuItem.Click += new System.EventHandler(this.zapiszToolStripMenuItem_Click);
+            // 
+            // edycjaToolStripMenuItem
+            // 
+            this.edycjaToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.cofnijToolStripMenuItem,
+            this.ponówToolStripMenuItem});
+            this.edycjaToolStripMenuItem.Name = "edycjaToolStripMenuItem";
+            this.edycjaToolStripMenuItem.Size = new System.Drawing.Size(53, 20);
+            this.edycjaToolStripMenuItem.Text = "Edycja";
+            // 
+            // cofnijToolStripMenuItem
+            // 
+            this.cofnijToolStripMenuItem.Name = "cofnijToolStripMenuItem";
+            this.cofnijToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cofnijToolStripMenuItem.Text = "Cofnij";
+            this.cofnijToolStripMenuItem.Click += new System.EventHandler(this.cofnijToolStripMenuItem_Click);
+            // 
+            // ponówToolStripMenuItem
+            // 
+            this.ponówToolStripMenuItem.Name = "ponówToolStripMenuItem";
+            this.ponówToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ponówToolStripMenuItem.Text = "Ponów";
+            this.ponówToolStripMenuItem.Click += new System.EventHandler(this.ponówToolStripMenuItem_Click);
+            // 
+            // FormPaint
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(862, 457);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "FormPaint";
+            this.Text = "Form1";
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.FormPaint_MouseMove);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.panel1.ResumeLayout(false);
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox3.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownWidthPen)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxImage)).EndInit();
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBoxImage;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton radioButtonCurve;
+        private System.Windows.Forms.RadioButton radioButtonEllipse;
+        private System.Windows.Forms.RadioButton radioButtonRectangle;
+        private System.Windows.Forms.RadioButton radioButtonLine;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBoxFill;
+        private System.Windows.Forms.Button buttonColorFill;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numericUpDownWidthPen;
+        private System.Windows.Forms.Button buttonColorPen;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem plikToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem nowyToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem otwórzToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem zapiszToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem edycjaToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cofnijToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem ponówToolStripMenuItem;
+    }
+}
+
---
+++ PK_06_Paint/FormPaint.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PK_06_Paint
+{
+    public partial class FormPaint : Form
+    {
+        List<Point> points = new List<Point>();
+        Graphics graphics;
+
+        List<Image> history;
+        public FormPaint()
+        {
+            InitializeComponent();
+            nowyToolStripMenuItem_Click(null, null);
+        }
+        private void FormPaint_MouseMove(object sender, MouseEventArgs e)
+        {
+            this.Text = e.Location.ToString() + e.Button;
+        }
+        private void pictureBoxImage_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                points = new List<Point>();
+                points.Add(e.Location);
+
+                while (undoCount > 0)
+                {
+                    history.Remove(history.Last());
+                    undoCount--;
+                }
+            }
+        }
+        private void pictureBoxImage_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                points.Add(e.Location);
+                Pen myPen = new Pen(buttonColorPen.BackColor, (int)numericUpDownWidthPen.Value);
+                Brush myBrush = new SolidBrush(buttonColorFill.BackColor);
+
+                Draw(myPen, myBrush);
+
+                history.Add(new Bitmap(pictureBoxImage.Image));
+                pictureBoxImage.Refresh();
+            }
+        }
+
+        private void Draw(Pen myPen, Brush myBrush)
+        {
+            if (radioButtonLine.Checked)
+            {
+                graphics.DrawLine(myPen, points.First(), points.Last());
+            }
+            else if (radioButtonRectangle.Checked)
+            {
+                if (checkBoxFill.Checked)
+                {
+                    graphics.FillRectangle(myBrush,
+                                           Math.Min(points.First().X, points.Last().X),
+                                           Math.Min(points.First().Y, points.Last().Y),
+                                           Math.Abs(points.First().X - points.Last().X),
+                                           Math.Abs(points.First().Y - points.Last().Y));
+                }
+                graphics.DrawRectangle(myPen,
+                                       Math.Min(points.First().X, points.Last().X),
+                                       Math.Min(points.First().Y, points.Last().Y),
+                                       Math.Abs(points.First().X - points.Last().X),
+                                       Math.Abs(points.First().Y - points.Last().Y));
+            }
+            else if (radioButtonEllipse.Checked)
+            {
+                if (checkBoxFill.Checked)
+                {
+                    graphics.FillEllipse(myBrush,
+                                         Math.Min(points.First().X, points.Last().X),
+                                         Math.Min(points.First().Y, points.Last().Y),
+                                         Math.Abs(points.First().X - points.Last().X),
+                                         Math.Abs(points.First().Y - points.Last().Y));
+                }
+                graphics.DrawEllipse(myPen,
+                                     Math.Min(points.First().X, points.Last().X),
+                                     Math.Min(points.First().Y, points.Last().Y),
+                                     Math.Abs(points.First().X - points.Last().X),
+                                     Math.Abs(points.First().Y - points.Last().Y));
+            }
+            else if (radioButtonCurve.Checked)
+            {
+                graphics.DrawCurve(myPen, points.ToArray());
+            }
+        }
+
+        private void pictureBoxImage_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                points.Add(e.Location);
+
+                pictureBoxImage.Image = new Bitmap(history.Last());
+                graphics = Graphics.FromImage(pictureBoxImage.Image);
+
+                Pen myPen = new Pen(Color.Black, (int)numericUpDownWidthPen.Value);
+                Brush myBrush = new SolidBrush(Color.LightGray);
+                Draw(myPen, myBrush);
+
+                pictureBoxImage.Refresh();
+            }
+        }
+
+        private void buttonColor_Click(object sender, EventArgs e)
+        {
+            ColorDialog cd = new ColorDialog();
+            cd.Color = (sender as Button).BackColor;
+            if (cd.ShowDialog() == DialogResult.OK)
+            {
+                (sender as Button).BackColor = cd.Color;
+            }
+        }
+
+        private void nowyToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            pictureBoxImage.Image = new Bitmap(pictureBoxImage.Width, pictureBoxImage.Height);
+            graphics = Graphics.FromImage(pictureBoxImage.Image);
+            graphics.Clear(Color.White);
+            history = new List<Image>();
+            undoCount = 0;
+            history.Add(new Bitmap(pictureBoxImage.Image));
+        }
+
+        private void otwórzToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Bitmapa|*.bmp|JPEG|*.jpg";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                pictureBoxImage.Image = new Bitmap(ofd.FileName);
+                graphics = Graphics.FromImage(pictureBoxImage.Image);
+                history = new List<Image>();
+                undoCount = 0;
+                history.Add(new Bitmap(pictureBoxImage.Image));
+            }
+        }
+
+        private void zapiszToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Bitmapa|*.bmp|JPEG|*.jpg";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                String extension = Path.GetExtension(sfd.FileName);
+                switch (extension)
+                {
+                    case ".bmp":
+                        pictureBoxImage.Image.Save(sfd.FileName, ImageFormat.Bmp);
+                        break;
+                    case ".jpg":
+                        pictureBoxImage.Image.Save(sfd.FileName, ImageFormat.Jpeg);
+                        break;
+
+                }
+            }
+        }
+
+        int undoCount = 0;
+        private void cofnijToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (undoCount < history.Count - 1)
+            {
+                undoCount++;
+                pictureBoxImage.Image = new Bitmap(history[history.Count - 1 - undoCount]);
+                graphics = Graphics.FromImage(pictureBoxImage.Image);
+            }
+        }
+
+        private void ponówToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (undoCount > 0)
+            {
+                undoCount--;
+                pictureBoxImage.Image = new Bitmap(history[history.Count - 1 - undoCount]);
+                graphics = Graphics.FromImage(pictureBoxImage.Image);
+            }
+        }
+    }
+}
